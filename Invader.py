cb517d5c04fd9ef127c1c58115404e39d6293a5b
IncrediblyOriginalUsername <alexroberts@dpengineering.org>
2019-10-01 09:36:09 -0700

    Added enemy death

--- Invader.py
+++ Invader.py
@@ -62,6 +62,9 @@ class MainScreen(Screen):
     global array
     global array2
     global lives
+    global bad
+    bad = 0
+    global array3
     lives = 5
     array = []
     array2 = []
@@ -70,6 +73,12 @@ class MainScreen(Screen):
         super(MainScreen, self).__init__(**kwargs)
         global event1
         global event2
+        global array3
+        array3 = [self.ids.bdgy0, self.ids.bdgy2, self.ids.bdgy3, self.ids.bdgy4, self.ids.bdgy5, self.ids.bdgy6,
+                  self.ids.bdgy7, self.ids.bdgy8, self.ids.bdgy9, self.ids.bdgy10, self.ids.bdgy11, self.ids.bdgy12,
+                  self.ids.bdgy13, self.ids.bdgy14, self.ids.bdgy15, self.ids.bdgy16, self.ids.bdgy17, self.ids.bdgy18,
+                  self.ids.bdgy19, self.ids.bdgy20, self.ids.bdgy21, self.ids.bdgy22, self.ids.bdgy23, self.ids.bdgy24,
+                  self.ids.bdgy25, self.ids.bdgy26, self.ids.bdgy27]
         event1 = Clock.schedule_interval(self.fire, 1/4)
         event2 = Clock.schedule_interval(self.fire2, 1/5)
         # I just used clock scheduling to see if it would work but you turn this into a thread instead if you want. if the fire function needs to be canceled, do event1.cancel()
@@ -90,14 +99,30 @@ class MainScreen(Screen):
             x = "%d" % xin
 
     def moveup(self):
+        global array3
+        global array
+        global bad
         for labels in array:
-            if(labels.y > 290):
+            if(labels.y > 300):
                 array.remove(labels)
                 self.remove_widget(labels)
+            for riseups in array3:
+                if (abs(labels.y - (riseups.y)) < 20 and abs(labels.x - riseups.x) < 20):
+                    self.remove_widget(riseups)
+                    array3.remove(riseups)
+                    self.remove_widget(labels)
+                    bad = 1
+            if(bad == 1):
+                array.remove(labels)
+                bad = 0
+
+
+
             labels.y = labels.y + 10
 
     def moveup2(self):
         global lives
+        global array2
         for labels2 in array2:
             if(labels2.y < -250):
                 array2.remove(labels2)
@@ -107,8 +132,8 @@ class MainScreen(Screen):
                 labels2.text = ">><<"
                 labels2.color = (1,0,0,1)
                 sleep(1/10)
-                array2.remove(labels2)
                 self.remove_widget(labels2)
+                array2.remove(labels2)
                 print("u were hit")
                 if(lives <= 0):
                     self.ids.spaceship.y = -1000
@@ -124,7 +149,7 @@ class MainScreen(Screen):
             self.add_widget(labels)
     def fire2(self,dt):
         global array3
-        array3 = [self.ids.bdgy1, self.ids.bdgy2, self.ids.bdgy3, self.ids.bdgy4, self.ids.bdgy5, self.ids.bdgy6, self.ids.bdgy7, self.ids.bdgy8, self.ids.bdgy9, self.ids.bdgy10, self.ids.bdgy11, self.ids.bdgy12, self.ids.bdgy13, self.ids.bdgy14, self.ids.bdgy15, self.ids.bdgy16, self.ids.bdgy17, self.ids.bdgy18, self.ids.bdgy19, self.ids.bdgy20, self.ids.bdgy21, self.ids.bdgy22, self.ids.bdgy23, self.ids.bdgy24, self.ids.bdgy25, self.ids.bdgy26, self.ids.bdgy27]
+
         global array2
         # print("fired")
         #Basic random shooting. In the future, we should automatically create a series of identical "ships" aka labels and put them in the array instead of
